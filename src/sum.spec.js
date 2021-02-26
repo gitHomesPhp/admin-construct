98fabac09b6f83932d9ac5b9d719873b56ee09d7
Evgen <evgeniy.kz@zivitech.com>
2021-02-26 16:23:39 +0300

    dev test - try write test

---
+++ src/sum.spec.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./index");
+describe("sum", function () {
+    it("sums two numbers", function () {
+        expect(index_1.sum(1, 2)).toEqual(3);
+    });
+});
